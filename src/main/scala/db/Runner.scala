8f0fe78432a19b37c2f7319410fe8ad3e44e113d
haghard <haghard84@gmail.com>
2019-04-20 19:42:52 +0300

    ClusterDomainEvents work

--- src/main/scala/db/Runner.scala
+++ src/main/scala/db/Runner.scala
@@ -1,9 +1,9 @@
 package db
 
-import akka.actor.ActorSystem
 import akka.cluster.Cluster
 import com.typesafe.config.ConfigFactory
-import db.core.{ DB, KeyValueStorageBackend2 }
+import db.core.{ DbReplica, KeyValueStorageBackend2 }
+
 import scala.concurrent.duration._
 
 //runMain db.Runner
@@ -28,7 +28,7 @@ object Runner extends App {
           actor.provider = cluster
 
           remote.artery.enabled = true
-          remote.artery.transport = tcp
+          #remote.artery.transport = tcp
           remote.artery.canonical.hostname = 127.0.0.1
 
           db-io {
@@ -56,29 +56,37 @@ object Runner extends App {
   val CL = 3
   val ticketNmr = 500000
 
-  val alphaSys = ActorSystem(systemName, portConfig(2550).withFallback(config).withFallback(ConfigFactory.load()))
-  val bettaSys = ActorSystem(systemName, portConfig(2551).withFallback(config).withFallback(ConfigFactory.load()))
-  val gammaSys = ActorSystem(systemName, portConfig(2552).withFallback(config).withFallback(ConfigFactory.load()))
+  import akka.actor.typed.scaladsl.adapter._
 
-  val alpha = Cluster(alphaSys)
-  val betta = Cluster(bettaSys)
-  val gamma = Cluster(gammaSys)
+  val alphaSys = akka.actor.typed.ActorSystem(
+    DbReplica(RF, CL, 0l), systemName,
+    portConfig(2550).withFallback(config).withFallback(ConfigFactory.load()))
+
+  val bettaSys = akka.actor.typed.ActorSystem(
+    DbReplica(RF, CL, 1l), systemName,
+    portConfig(2551).withFallback(config).withFallback(ConfigFactory.load()))
+
+  val gammaSys = akka.actor.typed.ActorSystem(
+    DbReplica(RF, CL, 2l), systemName,
+    portConfig(2552).withFallback(config).withFallback(ConfigFactory.load()))
+
+  val alpha = Cluster(alphaSys.toUntyped)
+  val betta = Cluster(bettaSys.toUntyped)
+  val gamma = Cluster(gammaSys.toUntyped)
 
   alpha.join(alpha.selfAddress)
   betta.join(alpha.selfAddress)
   gamma.join(alpha.selfAddress)
 
-  Helpers.waitForAllNodesUp(alphaSys, bettaSys, gammaSys)
+  Helpers.waitForAllNodesUp(alphaSys.toUntyped, bettaSys.toUntyped, gammaSys.toUntyped)
 
-  alphaSys.actorOf(DB.props(alpha, 0l, RF, CL), "alpha")
-  bettaSys.actorOf(DB.props(betta, 1l, RF, CL), "betta")
-  gammaSys.actorOf(DB.props(gamma, 2l, RF, CL), "gamma")
-
+  /*
   alphaSys.actorOf(KeyValueStorageBackend2.props, DB.PathSegment)
   bettaSys.actorOf(KeyValueStorageBackend2.props, DB.PathSegment)
   gammaSys.actorOf(KeyValueStorageBackend2.props, DB.PathSegment)
+  */
 
-  Helpers.wait(20.second)
+  /*Helpers.wait(20.second)
 
   println("****************** Kill gamma *********************")
   gamma.leave(gamma.selfAddress)
@@ -93,22 +101,18 @@ object Runner extends App {
   gammaSys2.actorOf(DB.props(gamma2, 2l, RF, CL), "gamma")
   gammaSys2.actorOf(KeyValueStorageBackend2.props, DB.PathSegment)
 
-  Helpers.waitForAllNodesUp(alphaSys, bettaSys, gammaSys2)
+  Helpers.waitForAllNodesUp(alphaSys, bettaSys, gammaSys2)*/
 
-  Helpers.wait(30.second)
+  Helpers.wait(10.second)
+
+  gamma.leave(gamma.selfAddress)
+  gammaSys.terminate
+
+  Helpers.wait(20.second)
 
   alpha.leave(alpha.selfAddress)
   alphaSys.terminate
 
   betta.leave(betta.selfAddress)
   bettaSys.terminate
-
-  /*
-  node3Cluster.leave(node3Cluster.selfAddress)
-  node3.terminate
-  */
-
-  gamma2.leave(gamma2.selfAddress)
-  gammaSys2.terminate
-
 }
